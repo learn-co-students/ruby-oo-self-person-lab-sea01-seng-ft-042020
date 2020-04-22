4f9b515ecfce9a936d7a7059a64bd87e98e742af
matthewsedlacek <sedlacek1991@gmail.com>
2020-04-22 11:29:54 -0700

    Matthew Driver and Andy Navigator

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,64 @@
 # your code goes here
+
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name 
+    def initialize (name)
+        @name = name 
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def name
+        @name 
+    end
+    def bank_account
+        @bank_account
+    end
+    def happiness 
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0 
+            @happiness = 0
+        else
+            @happiness
+        end
+    end
+    def hygiene
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0 
+            @hygiene = 0
+        else
+            @hygiene
+        end 
+    end
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid (salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        @hygiene += 4
+        self.hygiene=@hygiene
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+
+end
