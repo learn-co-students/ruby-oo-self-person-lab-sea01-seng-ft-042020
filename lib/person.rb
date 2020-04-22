50988cb799c46ac4c458d0d0abe0752fcd004091
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-22 11:14:58 -0700

    justin commit #1

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
-# your code goes here
+require 'pry'
+
+class Person 
+    def initialize(name,bank_account=25,happiness=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+    end
+    
+    attr_reader :name, :happiness
+    attr_accessor :bank_account
+
+    def happiness=(happiness)
+        if @happiness > 10
+            binding.pry
+
+            return nil
+        end
+    end
+end
+justin = Person.new("justin")
+justin.happiness
