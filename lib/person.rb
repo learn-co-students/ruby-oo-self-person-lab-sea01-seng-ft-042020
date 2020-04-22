c0818df34cd29c8801c61c8b5367f46ee5d9b1c4
sap2411 <steven.parsons.us@gmail.com>
2020-04-22 11:22:30 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,7 @@
 class Person
     attr_accessor :bank_account
     attr_reader :name, :hygiene, :happiness
+    
     def initialize(name)
         @name = name
         @bank_account = 25
@@ -39,37 +40,37 @@ class Person
 
     def get_paid(salary)
         self.bank_account += salary
-        return "all about the benjamins"
+        "all about the benjamins"
     end
 
     def take_bath
         self.hygiene += 4
-        return "♪ Rub-a-dub just relaxing in the tub ♫"
+        "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
         self.hygiene -= 3
         self.happiness += 2
-        return  "♪ another one bites the dust ♫"
+        "♪ another one bites the dust ♫"
     end
 
     def call_friend(friend)
         self.happiness += 3
         friend.happiness += 3
-        return  "Hi #{friend.name}! It's #{self.name}. How are you?"
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
     def start_conversation(friend, topic)
         if topic == 'politics'
             self.happiness -= 2
             friend.happiness -= 2
-            return 'blah blah partisan blah lobbyist'
+            'blah blah partisan blah lobbyist'
         elsif topic == 'weather'
             self.happiness += 1
             friend.happiness += 1
-            return 'blah blah sun blah rain'
+            'blah blah sun blah rain'
         else
-            return 'blah blah blah blah blah'
+            'blah blah blah blah blah'
         end
     end
 end
