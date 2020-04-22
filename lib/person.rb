06f965cc09a2a99039c4f056b2e3d0dc111282e3
sap2411 <steven.parsons.us@gmail.com>
2020-04-22 11:16:35 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -2,10 +2,10 @@
 class Person
     attr_accessor :bank_account
     attr_reader :name, :hygiene, :happiness
-    def initialize(name, happiness = 8)
+    def initialize(name)
         @name = name
         @bank_account = 25
-        @happiness = happiness
+        @happiness = 8
         @hygiene = 8
     end
 
@@ -30,49 +30,43 @@ class Person
     end
 
     def happy?
-        if @happiness > 7
-           return true
-        end
-        false 
+        self.happiness >7? true:false
     end
 
     def clean?
-        if @hygiene > 7
-            return true
-        end
-        false
+        self.hygiene >7? true:false
     end
 
     def get_paid(salary)
-        @bank_account += salary
+        self.bank_account += salary
         return "all about the benjamins"
     end
 
     def take_bath
-        self.hygiene=(@hygiene+4)
+        self.hygiene += 4
         return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.hygiene=(@hygiene-3)
-        self.happiness=(@happiness+2)
+        self.hygiene -= 3
+        self.happiness += 2
         return  "♪ another one bites the dust ♫"
     end
 
     def call_friend(friend)
-        self.happiness=(@happiness+3)
-        friend.happiness = (friend.happiness + 3)
+        self.happiness += 3
+        friend.happiness += 3
         return  "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
     def start_conversation(friend, topic)
         if topic == 'politics'
-            self.happiness= (@happiness-2)
-            friend.happiness= (friend.happiness-2)
+            self.happiness -= 2
+            friend.happiness -= 2
             return 'blah blah partisan blah lobbyist'
         elsif topic == 'weather'
-            self.happiness= (@happiness+1)
-            friend.happiness= (friend.happiness+1)
+            self.happiness += 1
+            friend.happiness += 1
             return 'blah blah sun blah rain'
         else
             return 'blah blah blah blah blah'
